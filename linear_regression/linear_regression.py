2ea5eb79d8036e68d053eaeba5e0bd9cb500c8db
Marek Kancir <kancir.marek@gmail.com>
2020-05-14 17:58:09 +0200

    add update of weights

--- linear_regression/linear_regression.py
+++ linear_regression/linear_regression.py
@@ -8,18 +8,27 @@ Created on Thu May 14 16:44:43 2020
 import numpy as np
 
 class LinearRegression():
-    def __init__(self, x, y, alpha=0.5, max_iter=100):
+    def __init__(self, x, y, learning_rate = 0.1, reg_lambda=0.5, max_iter=100):
         x = np.array(x)
-        self.m = x.shape[0]
+        self.m = len(y)
         self.x = np.c_[np.ones((self.m, 1)), x]
         self.y = np.array(y)
-        self.alpha = alpha
+        self.reg_lambda = reg_lambda
         self.iter = max_iter
+        self.lr = learning_rate
         self.w = np.random.normal(size=(self.x.shape[1], 1))
         
     def cost(self):
-        c = 0.5*self.m * (np.dot(self.x, self.w) - self.y)**2
-        return sum(c)
+        c = 1/(2*self.m) * (self.x @ self.w - self.y)**2
+        regC = c + 1/(2*self.m) * self.reg_lambda * self.w.transpose() @ self.w
+        return sum(regC)
+    
+    def update(self):
+        pred = self.x @ self.w
+        grad0 = self.lr * 1/self.m * sum(pred - self.y)*self.w[0]
+        grad1 = self.lr * 1/self.m * sum(pred - self.y)*self.w[1:]
+        self.w[0] = self.w[0] - grad0
+        self.w[1:] = self.w[1:]*(1 - self.lr * self.reg_lambda/self.m) - grad1
 
 if __name__=='__main__':
     x = np.random.normal(size=(500, 5))
