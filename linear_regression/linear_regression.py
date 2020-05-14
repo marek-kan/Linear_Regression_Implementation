53d6357764d2110bb973bc6d1343b4de9f93d972
Marek Kancir <kancir.marek@gmail.com>
2020-05-14 17:14:15 +0200

    add cost

--- linear_regression/linear_regression.py
+++ linear_regression/linear_regression.py
@@ -10,11 +10,16 @@ import numpy as np
 class LinearRegression():
     def __init__(self, x, y, alpha=0.5, max_iter=100):
         x = np.array(x)
-        self.x = np.c_[np.ones((x.shape[0], 1)), x]
+        self.m = x.shape[0]
+        self.x = np.c_[np.ones((self.m, 1)), x]
         self.y = np.array(y)
         self.alpha = alpha
         self.iter = max_iter
-        self.w = np.random.normal(size=(x.shape[1], 1))
+        self.w = np.random.normal(size=(self.x.shape[1], 1))
+        
+    def cost(self):
+        c = 0.5*self.m * (np.dot(self.x, self.w) - self.y)**2
+        return sum(c)
 
 if __name__=='__main__':
     x = np.random.normal(size=(500, 5))
