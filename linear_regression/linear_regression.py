53f2a66a78f290f253943b2e68948f80afb3ce27
Marek Kancir <kancir.marek@gmail.com>
2020-05-15 11:40:24 +0200

    add gradient with momentum

--- linear_regression/linear_regression.py
+++ linear_regression/linear_regression.py
@@ -8,10 +8,11 @@ Created on Thu May 14 16:44:43 2020
 import numpy as np
 
 class LinearRegression():
-    def __init__(self, learning_rate = 0.1, reg_lambda=0.5, max_iter=100):
+    def __init__(self, learning_rate = 0.1, beta=0.9, reg_lambda=0.5, max_iter=100):
         self.reg_lambda = reg_lambda
         self.iter = max_iter
         self.lr = learning_rate
+        self.b = beta
         
     def cost(self, x, y, m):
         c = 1/(2*m) * (x @ self.w - y)**2
@@ -22,8 +23,12 @@ class LinearRegression():
         pred = x @ self.w
         grad0 = 1/m * (x[:, 0].T @ (pred - y))
         grad1 = 1/m * (x[:, 1:].T @ (pred - y))
-        self.w[0] = self.w[0] - self.lr * grad0
-        self.w[1:] = self.w[1:]*(1 - self.lr * self.reg_lambda/m) - self.lr * grad1
+        # compute accelerations for bias and coefficients
+        self.v[0] = self.b*self.v[0] + (1-self.b)*grad0
+        self.v[1:] = self.b*self.v[1:] + (1-self.b)*grad1
+        # Updating the weights
+        self.w[0] = self.w[0] - self.lr * self.v[0]
+        self.w[1:] = self.w[1:]*(1 - self.lr * self.reg_lambda/m) - self.lr * self.v[1:]
         
     def fit(self, x, y):
         x = np.array(x)
@@ -31,12 +36,13 @@ class LinearRegression():
         x = np.c_[np.ones((m, 1)), x]
         y = np.array(y).reshape(m, 1)
         self.w = np.random.normal(size=(x.shape[1], 1))
+        self.v = np.zeros(shape=(x.shape[1], 1)) # initialize momentum acceleration to zeros
         self.costs = []
         for i in range(self.iter):
             self.update(x, y, m)
             self.costs.append(self.cost(x, y, m)[0])
         return f'Final cost {self.costs[-1]}'
-    
+       
     def predict(self, x):
         x = np.array(x)
         x = np.c_[np.ones((x.shape[0])), x]
