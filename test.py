7fa974c0865b5e591eb582ca387251710c245f9b
Marek Kancir <kancir.marek@gmail.com>
2020-05-15 12:25:51 +0200

    compare real dataset

--- test.py
+++ test.py
@@ -6,27 +6,36 @@ Created on Thu May 14 18:50:03 2020
 """
 
 
-from sklearn.datasets import make_regression
-from sklearn.linear_model import LinearRegression as LR
+from sklearn.datasets import load_boston
+from sklearn.linear_model import Lasso
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 
 from linear_regression import linear_regression as lr
 
-x, y = make_regression(n_samples = 1500, n_features=20, n_informative=6)
+x, y = load_boston(return_X_y=True)
+scaler = StandardScaler()
+
 x_tr, x_te, y_tr, y_te = train_test_split(x, y, test_size=0.1, random_state=5, shuffle=True)
-lin = lr.LinearRegression(max_iter=1000, reg_lambda=0)
+x_tr = scaler.fit_transform(x_tr)
+x_te = scaler.transform(x_te)
+
+lin = lr.LinearRegression(max_iter=5000, learning_rate=0.01, reg_lambda=0, beta=0.75)
 lin.fit(x_tr, y_tr)
 loss = lin.costs
 
-sk = LR()
+sk = Lasso(max_iter=5000, alpha=5e-5)
 sk.fit(x_tr, y_tr)
 
 mae1 = mean_absolute_error(y_te, lin.predict(x_te))
 mae2 = mean_absolute_error(y_te, sk.predict(x_te))
 
 plt.plot(range(len(loss)), loss)
+plt.title('Training loss')
+plt.show()
+plt.close()
 
 # Test "online learning"
 new_examples = 5
@@ -35,3 +44,6 @@ for i in range(new_examples):
     lin.fit_sample(x_te[i, :].reshape(1, x.shape[1]), y_te[i])
     online_mae.append(mean_absolute_error(y_te[i+1:], lin.predict(x_te[i+1:, :])))
     
+plt.plot(range(len(online_mae)), online_mae)
+plt.title('MAE after online learning')
+plt.show()
