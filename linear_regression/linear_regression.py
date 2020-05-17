988a496cc9ced2250a267fc4ff405b0f8d7fc78c
Marek Kancir <kancir.marek@gmail.com>
2020-05-17 11:16:04 +0200

    add procedure for R^2, adj. R^2 and P-Values/T-stat

--- linear_regression/linear_regression.py
+++ linear_regression/linear_regression.py
@@ -6,6 +6,7 @@ Created on Thu May 14 16:44:43 2020
 """
 
 import numpy as np
+import scipy.stats as stat
 
 class LinearRegression():
     def __init__(self, learning_rate = 0.1, beta=0.9, reg_lambda=0.5, max_iter=100):
@@ -41,6 +42,7 @@ class LinearRegression():
         for i in range(self.iter):
             self.update(x, y, m)
             self.costs.append(self.cost(x, y, m)[0])
+        self.calculate_stat(x, y, training=True)
         return print(f'Final loss {self.costs[-1]}')
     
     def fit_sample(self, x, y, iterations=1):
@@ -63,6 +65,53 @@ class LinearRegression():
         x = np.array(x)
         x = np.c_[np.ones((x.shape[0])), x]
         return np.dot(x, self.w)
+    
+    def r2(self, x, y, training=False):
+        """
+        training=False, calculate on test set
+        """
+        if not training:
+            pred = self.predict(x)
+            y = np.array(y)
+            y = y.reshape(y.shape[0], 1)
+        else:
+            pred = self.predict(x[:, 1:])
+        # model variance
+        mod_var = sum((pred - y)**2)
+        # total variance
+        tot_var = sum((y.mean() - y)**2)
+        # variance explained by model
+        exp_var = tot_var - mod_var
+        return (exp_var/tot_var)[0]
+        
+    
+    def calculate_stat(self, x, y, training=False):
+        """
+        training=False, calculate on test set
+        """
+        if not training:
+            pred = self.predict(x)
+            x = np.array(x)
+            x = np.c_[np.ones((x.shape[0])), x]
+            y = np.array(y)
+            y = y.reshape(y.shape[0], 1)
+        else:
+            pred = self.predict(x[:, 1:])
+        # Calculate SSE (sum of squared errors) and SE (standard error)
+        sse = np.sum((pred - y) ** 2) / float(x.shape[0] - x.shape[1])
+        se = np.array([np.sqrt(np.diagonal(sse * np.linalg.inv(np.dot(x.T, x))))])
+        # Compute the t-statistic
+        self.t_stat = self.w.reshape(self.w.shape[0]) / se
+        # Find the p-value for each feature
+        self.p_values = np.squeeze(2 * (1 - stat.t.cdf(np.abs(self.t_stat), y.shape[0] - x.shape[1])))
+        #adjusted R^2, True because I added ones before
+        if not training:
+            # only return adjusted R^2 on test set
+            adj_score = (1-(1-self.r2(x, y, True))*(x.shape[0]-1)/(x.shape[0]-x.shape[1]-1))
+            return adj_score
+        else:
+            self.adj_score = (1-(1-self.r2(x, y, True))*(x.shape[0]-1)/(x.shape[0]-x.shape[1]-1))
+        
 
 
     
